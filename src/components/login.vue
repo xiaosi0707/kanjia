37ce575f5c6b1f3094b2bf53c368894b04f171a0
wyunfei <583514368@qq.com>
2018-11-23 16:45:25 +0800

    登录逻辑

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,8 @@
 <template>
     <div class="login">
-
         <el-card shadow="always">
           登录
         </el-card>
-
       <div>
         <el-input placeholder="请输入token" v-model="input3">
           <template slot="prepend">token</template>
@@ -12,12 +10,40 @@
         <!--<el-input placeholder="请输入内容" v-model="input3">-->
           <!--<template slot="prepend">密码</template>-->
         <!--</el-input>-->
-        <el-button type="danger">登录</el-button>
+        <el-button type="danger" @click="login" :loading="isLoading">登录</el-button>
       </div>
     </div>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      isLoading: false
+    }
+  },
+  methods: {
+    open6 () {
+      this.$message({
+        showClose: true,
+        message: '恭喜你，登陆成功！',
+        type: 'success'
+      })
+    },
+    login () {
+      this.isLoading = true
+      setTimeout(() => {
+        this.open6()
+        setTimeout(() => {
+          this.$router.push({
+            path: 'kanjia'
+          })
+        }, 2000)
+
+      }, 2000)
+    }
+  }
+}
 </script>
 
 <style scoped>
