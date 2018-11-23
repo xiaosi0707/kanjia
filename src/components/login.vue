ec64d92ba626d547e9bef049e2799c81141e2e65
wyunfei <583514368@qq.com>
2018-11-23 15:57:46 +0800

    登录组件编写完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,11 +1,23 @@
 <template>
-    
+    <div class="login">
+
+        <el-card shadow="always">
+          登录
+        </el-card>
+
+      <div>
+        <el-input placeholder="请输入内容" v-model="input3">
+          <template slot="prepend">账号</template>
+        </el-input>
+        <el-input placeholder="请输入内容" v-model="input3">
+          <template slot="prepend">密码</template>
+        </el-input>
+        <el-button type="danger">登录</el-button>
+      </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'login'
-  }
 </script>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Detail from '@/components/detail'
 import Kanjia from '@/components/kanjia'
+import Login from '@/components/login'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/kanjia',
       name: 'Kanjia',
       component: Kanjia
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
