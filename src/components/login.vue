22b0bc6a3f9ef73ed82a35ac4992ed5446680f33
wyunfei <583514368@qq.com>
2018-11-23 16:01:30 +0800

    登录组件优化

--- src/components/login.vue
+++ src/components/login.vue
@@ -6,12 +6,12 @@
         </el-card>
 
       <div>
-        <el-input placeholder="请输入内容" v-model="input3">
-          <template slot="prepend">账号</template>
+        <el-input placeholder="请输入token" v-model="input3">
+          <template slot="prepend">token</template>
         </el-input>
-        <el-input placeholder="请输入内容" v-model="input3">
-          <template slot="prepend">密码</template>
-        </el-input>
+        <!--<el-input placeholder="请输入内容" v-model="input3">-->
+          <!--<template slot="prepend">密码</template>-->
+        <!--</el-input>-->
         <el-button type="danger">登录</el-button>
       </div>
     </div>
